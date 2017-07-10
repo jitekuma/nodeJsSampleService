b6d87d992f0b6674de8a07f0141ffe260bf12587
Jitendra Kumar <rockjiten@gmail.com>
2017-07-10 09:33:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', function (req, res) {
-  res.send('Hello world 6\n');
+  res.send('Hello world 7\n');
 });
 
 app.listen(PORT, HOST);
